a4aefd29ea7b19c28eb9b6e2d79acbc4836ca70a
Fayzulla Rahmatullayev <fayzullarakhmatullayev@gmail.com>
2022-10-16 16:20:35 +0500

    init app

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require("node-telegram-bot-api");
+const token = "5791242571:AAHjXTB14IepSDA1okeKFxTKtQTo1dgU0Kg";
+const webAppUrl = "https://ya.ru";
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
+      reply_markup: {
+        keyboard: [[{ text: "Заполнить форму", web_app: { url: webAppUrl } }]],
+      },
+    });
+
+    await bot.sendMessage(
+      chatId,
+      "Заходи в наш интернет магазин по кнопке ниже",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
+          ],
+        },
+      }
+    );
+  }
+});

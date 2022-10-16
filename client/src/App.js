214b473a340ca536929f29dec7f3aaf28e23087d
Fayzulla Rahmatullayev <fayzullarakhmatullayev@gmail.com>
2022-10-16 17:19:20 +0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import "./App.css";
 import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
-import "./App.css";
 import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
   const { tg, onToggleButton } = useTelegram();
@@ -12,7 +15,10 @@ function App() {
   return (
     <div>
       <Header />
-      <button onClick={onToggleButton}>Toggle</button>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={"form"} element={<Form />} />
+      </Routes>
     </div>
   );
 }

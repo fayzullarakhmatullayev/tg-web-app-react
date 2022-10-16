37a33b3fbc67a0c2c340ffc969ce7612441a5bed
Fayzulla Rahmatullayev <fayzullarakhmatullayev@gmail.com>
2022-10-16 18:38:17 +0500

    init

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -32,7 +32,12 @@ const ProductList = () => {
   return (
     <div className={"list"}>
       {products.map((item) => (
-        <ProductItem product={item} onAdd={onAdd} className={"item"} />
+        <ProductItem
+          key={item.id}
+          product={item}
+          onAdd={onAdd}
+          className={"item"}
+        />
       ))}
     </div>
   );
